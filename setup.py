606d2eafeb3585efc57a2b90db613ed0196182a2
Luchesar ILIEV <luchesar.iliev@gmail.com>
2022-01-11 14:44:06 +0200

    version: bump to 1.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fs:
 
 setup(
     name="pynanomapper",
-    version="1.0.4",
+    version="1.0.5",
     author="Nina JELIAZKOVA",
     author_email="jeliazkova.nina@gmail.com",
     description="eNanoMapper API client",
