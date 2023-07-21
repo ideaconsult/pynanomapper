b3f42992554c9f4f4c7a5be6f06956d4d7b0648c
vedina <jeliazkova.nina@gmail.com>
2023-07-21 12:27:28 +0300

    ambit metadata into nexus metadata

--- pynanomapper/datamodel/nexus_utils.py
+++ pynanomapper/datamodel/nexus_utils.py
@@ -16,32 +16,107 @@ import nexusformat.nexus as nx
 def to_nexus(papp : mx.ProtocolApplication, nx_root: nx.NXroot() = None ):
     if nx_root == None:
         nx_root = nx.NXroot()
+    #https://manual.nexusformat.org/classes/base_classes/NXentry.html
     entry_id = "entry_{}".format(papp.uuid)
     nx_root[entry_id] = nx.tree.NXentry()
+    nx_root['{}/entry_identifier_uuid'.format(entry_id)] = papp.uuid
+
+    nx_root['{}/definition'.format(entry_id)] = papp.__class__.__name__
+    #experiment_identifier
+    #experiment_description
+    #collection_identifier collection of related measurements or experiments.
+    nx_root['{}/collection_identifier'.format(entry_id)] = papp.investigation_uuid
+    nx_root['{}/experiment_identifier'.format(entry_id)] = papp.assay_uuid
+    #collection_description
+
+    #duration
+    #program_name
+    #revision
+    #experiment_documentation (SOP)
+    #notes
+    #USER: (optional) NXuser
+    #SAMPLE: (optional) NXsample
+    #INSTRUMENT: (optional) NXinstrument
+    #COLLECTION: (optional) NXcollection
+    #MONITOR: (optional) NXmonitor
+    #PARAMETERS: (optional) NXparameters Container for parameters, usually used in processing or analysis.
+    #PROCESS: (optional) NXprocess
+    #SUBENTRY: (optional) NXsubentry Group of multiple application definitions for “multi-modal” (e.g. SAXS/WAXS) measurements.
+
     if not (papp.protocol is None):
+        instrument_id = '{}/protocol'.format(entry_id)
         #papp.protocol.endpoint
         #papp.protocol.topcategory
         #papp.protocol.category
         #papp.protocol.guideline
-        pass
+        #experiment_documentation
+        experiment_documentation = nx.NXnote()
+        nx_root['{}/experiment_documentation'.format(entry_id)] = experiment_documentation
+
+        category = nx.NXgroup()
+        experiment_documentation["category"] = category
+        category.attrs["topcategory"] = papp.protocol.topcategory
+        category.attrs["code"] = papp.protocol.category.code
+        category.attrs["term"] = papp.protocol.category.term
+        category.attrs["title"] = papp.protocol.category.title
+        category.attrs["endpoint"] = papp.protocol.endpoint
+        for guide in papp.protocol.guideline:
+            experiment_documentation["guideline"] = papp.protocol.guideline
+            break
+
     if not (papp.citation is None):
-        #papp.citation.year
-        #papp.citation.title
-        #papp.citation.owner
-        pass
+        citation_id = '{}/reference'.format(entry_id)
+        nx_root[citation_id] = nx.NXcite()
+        nx_root[citation_id]["title"] = papp.citation.title
+        nx_root[citation_id]["year"] = papp.citation.year
+        nx_root[citation_id]["owner"] = papp.citation.owner
+        #url, doi, description
+
     if not (papp.parameters is None):
         instrument_id = '{}/instrument'.format(entry_id)
-        nx_root[instrument_id] = nx.NXinstrument();
+        params_id = '{}/parameters'.format(entry_id)
+        env_id = '{}/environment'.format(entry_id)
+        instrument = nx.NXinstrument()
+        parameters = nx.NXcollection()
+        environment = nx.NXenvironment()
+        sample = nx.tree.NXsample()
         for prm in papp.parameters:
             value = papp.parameters[prm]
-            if isinstance(value,str):
-                pass
+            target = environment
+            if  "instrument" in prm.lower():
+                target = instrument
+            elif  "sample" in prm.lower():
+                target = sample
+            elif  "material" in prm.lower():
+                target = sample
+            elif ("ASSAY" == prm.upper()) or ("E.METHOD" == prm.upper()):
+                target = instrument
+            elif ("E.SOP_REFERENCE" == prm):
+                target = instrument
+            elif ("OPERATOR" == prm):
+                target = instrument
+            elif (prm.startswith("T.")):
+                target = instrument
+            if "EXPERIMENT_END_DATE" == prm:
+                nx_root[entry_id]["end_time"] = value
+            elif "EXPERIMENT_START_DATE" == prm:
+                nx_root[entry_id]["start_time"] = value
+            elif isinstance(value,str):
+                target[prm] = nx.NXfield(str(value))
             elif isinstance(value,mx.Value):
                 #tbd ranges?
-                nx_root['{}/instrument/{}'.format(entry_id,prm)] = nx.NXfield(value.loValue,unit=value.unit)
-    nx_root['{}/title'.format(entry_id)] = 'Nexus deco method test'
+                target[prm] = nx.NXfield(value.loValue,unit=value.unit)
+        nx_root[instrument_id] = instrument
+        nx_root[params_id] = parameters
+        nx_root[env_id] = environment
+        nx_root['{}/sample'.format(entry_id)] = sample
+
     if not (papp.owner is None):
-        nx_root['{}/sample'.format(entry_id)] = nx.tree.NXsample()
+        try:
+            sample["uuid"] = papp.owner.substance.uuid
+            sample["provider"] = papp.owner.company.name
+        except:
+            pass
     return nx_root
 
 
