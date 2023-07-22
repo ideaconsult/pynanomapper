3829d18168ed1222abc65ace327a633afc0c68e7
vedina <jeliazkova.nina@gmail.com>
2023-07-22 10:17:30 +0300

    improved error handling

--- pynanomapper/datamodel/nexus_utils.py
+++ pynanomapper/datamodel/nexus_utils.py
@@ -8,19 +8,38 @@ import pandas as pd
 import re
 import traceback
 
-# Usage
-# from  pynanomapper.datamodel.nexus_utils import to_nexus
-# from  pynanomapper.datamodel.measurements import ProtocolApplication
-# pa = ProtocolApplication(**json_data)
-# import nexusformat.nexus.tree as nx
-# ne = pa.to_nexus(nx.NXroot())
-# print(ne.tree)
+
+"""
+    data model to nexus
+
+    Args:
+        papp (ProtocolApplication): The numerator.
+        nx_root (nx.NXroot()): Nexus root (or None).
+
+    Returns:
+        nx_root: Nexus root
+
+    Raises:
+        Exception: on parse
+
+    Examples:
+        from  pynanomapper.datamodel.nexus_utils import to_nexus
+        from  pynanomapper.datamodel.measurements import ProtocolApplication
+        pa = ProtocolApplication(**json_data)
+        import nexusformat.nexus.tree as nx
+        ne = pa.to_nexus(nx.NXroot())
+        print(ne.tree)
+"""
 @add_ambitmodel_method(mx.ProtocolApplication)
-def to_nexus(papp : mx.ProtocolApplication, nx_root: nx.NXroot() = None ):
+def to_nexus(papp : mx.ProtocolApplication, nx_root: nx.NXroot() = None ) :
     if nx_root == None:
         nx_root = nx.NXroot()
     #https://manual.nexusformat.org/classes/base_classes/NXentry.html
-    entry_id = "entry_{}".format(papp.uuid)
+    try:
+        entry_id = "entry_{}.{}_{}".format(papp.protocol.topcategory,papp.protocol.category.code,papp.uuid)
+    except:
+        entry_id = "entry_{}".format(papp.uuid)
+
     nx_root[entry_id] = nx.tree.NXentry()
     nx_root['{}/entry_identifier_uuid'.format(entry_id)] = papp.uuid
 
@@ -46,85 +65,87 @@ def to_nexus(papp : mx.ProtocolApplication, nx_root: nx.NXroot() = None ):
     #PROCESS: (optional) NXprocess
     #SUBENTRY: (optional) NXsubentry Group of multiple application definitions for “multi-modal” (e.g. SAXS/WAXS) measurements.
 
-    if not (papp.protocol is None):
-        instrument_id = '{}/protocol'.format(entry_id)
-        #papp.protocol.endpoint
-        #papp.protocol.topcategory
-        #papp.protocol.category
-        #papp.protocol.guideline
-        #experiment_documentation
-        experiment_documentation = nx.NXnote()
-        nx_root['{}/experiment_documentation'.format(entry_id)] = experiment_documentation
+    try:
+        if not (papp.protocol is None):
+            experiment_documentation = nx.NXnote()
+            nx_root['{}/experiment_documentation'.format(entry_id)] = experiment_documentation
 
-        category = nx.NXgroup()
-        experiment_documentation["category"] = category
-        category.attrs["topcategory"] = papp.protocol.topcategory
-        category.attrs["code"] = papp.protocol.category.code
-        category.attrs["term"] = papp.protocol.category.term
-        category.attrs["title"] = papp.protocol.category.title
-        category.attrs["endpoint"] = papp.protocol.endpoint
-        for guide in papp.protocol.guideline:
-            experiment_documentation["guideline"] = papp.protocol.guideline
-            break
+            category = nx.NXgroup()
+            experiment_documentation["category"] = category
+            category.attrs["topcategory"] = papp.protocol.topcategory
+            category.attrs["code"] = papp.protocol.category.code
+            category.attrs["term"] = papp.protocol.category.term
+            category.attrs["title"] = papp.protocol.category.title
+            category.attrs["endpoint"] = papp.protocol.endpoint
+            for guide in papp.protocol.guideline:
+                experiment_documentation["guideline"] = papp.protocol.guideline
+                break
+    except Exception as err:
+        raise Exception("protocol parsing error " + str(err)) from err
 
-    if not (papp.citation is None):
+    try:
         citation_id = '{}/reference'.format(entry_id)
         nx_root[citation_id] = nx.NXcite()
-        nx_root[citation_id]["title"] = papp.citation.title
-        nx_root[citation_id]["year"] = papp.citation.year
-        nx_root[citation_id]["owner"] = papp.citation.owner
+        if papp.citation != None:
+            nx_root[citation_id]["title"] = papp.citation.title
+            nx_root[citation_id]["year"] = papp.citation.year
+            nx_root[citation_id]["owner"] = papp.citation.owner
         #url, doi, description
+    except Exception as err:
+        raise Exception("citation data parsing error " + str(err)) from err
+
+    instrument = nx.NXinstrument()
+    parameters = nx.NXcollection()
+    environment = nx.NXenvironment()
+    sample = nx.tree.NXsample()
+    nx_root['{}/instrument'.format(entry_id)] = instrument
+    nx_root['{}/parameters'.format(entry_id)] = parameters
+    nx_root['{}/environment'.format(entry_id)] = environment
+    nx_root['{}/sample'.format(entry_id)] = sample
 
     if not (papp.parameters is None):
-        instrument_id = '{}/instrument'.format(entry_id)
-        params_id = '{}/parameters'.format(entry_id)
-        env_id = '{}/environment'.format(entry_id)
-        instrument = nx.NXinstrument()
-        parameters = nx.NXcollection()
-        environment = nx.NXenvironment()
-        sample = nx.tree.NXsample()
         for prm in papp.parameters:
-            value = papp.parameters[prm]
-            target = environment
-            if  "instrument" in prm.lower():
-                target = instrument
-            elif  "sample" in prm.lower():
-                target = sample
-            elif  "material" in prm.lower():
-                target = sample
-            elif ("ASSAY" == prm.upper()) or ("E.METHOD" == prm.upper()):
-                target = instrument
-            elif ("E.SOP_REFERENCE" == prm):
-                target = instrument
-            elif ("OPERATOR" == prm):
-                target = instrument
-            elif (prm.startswith("T.")):
-                target = instrument
-            if "EXPERIMENT_END_DATE" == prm:
-                nx_root[entry_id]["end_time"] = value
-            elif "EXPERIMENT_START_DATE" == prm:
-                nx_root[entry_id]["start_time"] = value
-            elif isinstance(value,str):
-                target[prm] = nx.NXfield(str(value))
-            elif isinstance(value,mx.Value):
-                #tbd ranges?
-                target[prm] = nx.NXfield(value.loValue,unit=value.unit)
-        nx_root[instrument_id] = instrument
-        nx_root[params_id] = parameters
-        nx_root[env_id] = environment
-        nx_root['{}/sample'.format(entry_id)] = sample
+            try:
+                value = papp.parameters[prm]
+                target = environment
+                if  "instrument" in prm.lower():
+                    target = instrument
+                elif  "sample" in prm.lower():
+                    target = sample
+                elif  "material" in prm.lower():
+                    target = sample
+                elif ("ASSAY" == prm.upper()) or ("E.METHOD" == prm.upper()):
+                    target = instrument
+                elif ("E.SOP_REFERENCE" == prm):
+                    target = instrument
+                elif ("OPERATOR" == prm):
+                    target = instrument
+                elif (prm.startswith("T.")):
+                    target = instrument
+                if "EXPERIMENT_END_DATE" == prm:
+                    nx_root[entry_id]["end_time"] = value
+                elif "EXPERIMENT_START_DATE" == prm:
+                    nx_root[entry_id]["start_time"] = value
+                elif isinstance(value,str):
+                    target[prm] = nx.NXfield(str(value))
+                elif isinstance(value,mx.Value):
+                    #tbd ranges?
+                    target[prm] = nx.NXfield(value.loValue,unit=value.unit)
+            except Exception as err:
+                raise Exception("papp parameters parsing error " + str(err)) from err
 
     if not (papp.owner is None):
         try:
             sample["uuid"] = papp.owner.substance.uuid
             sample["provider"] = papp.owner.company.name
-        except:
-            pass
+        except Exception as err:
+            raise Exception("papp.owner parsing error " + str(err)) from err
 
     try:
         process_pa(papp,nx_root[entry_id])
     except Exception as err:
-        print(err)
+        raise Exception("papp data parsing error " + str(err)) from err
+
     return nx_root
 
 
