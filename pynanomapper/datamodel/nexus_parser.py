a6a6ab30b083847b51f3705ae11c9d392c49237b
vedina <jeliazkova.nina@gmail.com>
2023-09-11 17:28:54 +0300

    prepare for supporting processed data

--- pynanomapper/datamodel/nexus_parser.py
+++ pynanomapper/datamodel/nexus_parser.py
@@ -4,21 +4,26 @@ class NexusParser:
     def __init__(self):
         self.parsed_objects = {}
 
-    def parse_data(self,entry,default=False):
+    def parse_data(self,entry,default=False,nxprocess=False):
         for attr in entry.attrs:
             print(attr,entry.attrs.get(attr))
         for name, item in entry.items():
             nx_class = item.attrs.get('NX_class', None)
-            print("DATA ",item.name, ' ', nx_class)
+            print("PROCESSED " if nxprocess else "","DATA ",item.name, ' ', nx_class)
 
-    def parse_entry(self,entry):
+    def parse_entry(self,entry,nxprocess=False,dataparser=None):
+        print(dataparser)
         nx_class = entry.attrs.get('NX_class', None)
         default = entry.attrs.get('default', None)
-        print(entry.name, ' ', nx_class, default)
+        #print(entry.name, ' ', nx_class, default)
         for name, item in entry.items():
             nx_class = item.attrs.get('NX_class', None)
             if nx_class == "NXdata":
-                self.parse_data(item,entry.name==default)
+                if dataparser is None:
+                    self.parse_data(item,entry.name==default,nxprocess)
+                else:
+                    print("dataparsre",dataparser)
+                    dataparser(item,entry.name==default,nxprocess)
 
             elif nx_class == "NXenvironment":
                 pass
@@ -42,16 +47,16 @@ class NexusParser:
         else:
             print(group.name, ' ', nx_class)
 
-    def parse(self,file_path  :str):
+    def parse(self,file_path  :str,dataparser=None):
         with h5py.File(file_path, 'r') as file:
-            self.parse_h5(file)
+            self.parse_h5(file,dataparser)
 
-    def parse_h5(self,h5_file):
+    def parse_h5(self,h5_file,dataparser=None):
         try:
-            def iterate_groups(group, indent=''):
+            def iterate_groups(group, indent='',nxprocess = False):
                 nx_class = group.attrs.get('NX_class', None)
                 if nx_class == "NXentry" or nx_class == "NXsubentry":
-                    self.parse_entry(group)
+                    self.parse_entry(group,nxprocess,dataparser)
                 elif nx_class == "NXsample":
                     self.parse_sample(group)
 
@@ -59,9 +64,9 @@ class NexusParser:
                     for name, item in group.items():
                         nx_class = item.attrs.get('NX_class', None)
                         if isinstance(item, h5py.Group):
-                            print(indent + 'Group:', name, ' ', nx_class)
+                            #print(indent + 'Group:', name, ' ', nx_class)
                             # Recursively call the function for nested groups
-                            iterate_groups(item, indent + '  ')
+                            iterate_groups(item, indent + '  ',nxprocess or nx_class=="NX_process")
                         else:
                             print(indent + 'Dataset:', name, ' ', nx_class)
 
@@ -76,7 +81,7 @@ class SpectrumParser(NexusParser):
         # Replace the parent class field with the spectrum-specific field
         self.parsed_objects = {}
 
-    def parse_data(self,entry,default=False):
+    def parse_data(self,entry,default=False,nxprocess=False):
 
         signal = entry.attrs.get('signal', None)
         interpretation = entry.attrs.get('interpretation', None)
