c28a013cbc889ba164e19fd400f9dc4544a3c151
vedina <jeliazkova.nina@gmail.com>
2024-01-27 15:18:26 +0200

    method metadata

--- pynanomapper/datamodel/templates/blueprint.py
+++ pynanomapper/datamodel/templates/blueprint.py
@@ -14,8 +14,31 @@ def iom_format(df,param_name="param_name",param_group="param_group"):
         result_df = pd.concat([result_df, pd.DataFrame({'param_name': [group] + names + ['']})], ignore_index=True)
     return result_df
 
-def json2frame(json_data,sortby):
-    return pd.DataFrame(json_data).sort_values(by=sortby)
+def json2frame(json_data,sortby=None):
+    tmp = pd.DataFrame(json_data)
+    if sortby is None:
+        return tmp
+    else:
+        return tmp.sort_values(by=sortby)
+
+def get_method_metadata(json_blueprint):
+    _header = {
+    "Project Work Package" : "",
+    "Partner conducting test/assay" : "",
+    "Test facility - Laboratory name" : "",
+    "Lead Scientist & contact for test" : "",
+    "Assay/Test work conducted by" : "",
+    "Full name of test/assay" : json_blueprint.get("EXPERIMENT",""),
+    "Short name or acronym for test/assay": json_blueprint.get("METHOD",""),
+    "Type or class of experimental test as used here": json_blueprint.get("PROTOCOL_CATEGORY_CODE",""),
+    "End-Point being investigated/assessed by the test" : [],
+    "Raw data metrics" : [],
+    "SOP(s) for test" : "",
+    "Path/link to sop/protocol": "",
+    "Test start date": None,
+    "Test end date": None
+    }
+    return _header
 
 def get_template_frame(json_blueprint):
     df_sample = json2frame(json_blueprint["METADATA_SAMPLE_INFO"],sortby=["param_sample_group"]).rename(columns={'param_sample_name': 'param_name'})
@@ -29,23 +52,54 @@ def get_template_frame(json_blueprint):
 
     #print(df_sample.columns,result_df.columns)
     empty_row = pd.DataFrame({col: [""] * len(result_df.columns) for col in result_df.columns})
-    return pd.concat([df_sample[["param_name"]],empty_row,result_df_sampleprep,empty_row,result_df], ignore_index=True)
 
-def iom_format_2excel(df,file_path):
+    df_method = pd.DataFrame(list(get_method_metadata(json_blueprint).items()), columns=['param_name', 'param_value'])
+    df_info =  pd.concat([
+        df_method[["param_name"]],
+        empty_row,
+        df_sample[["param_name"]],
+        empty_row,
+        result_df_sampleprep,
+        empty_row,
+        result_df
+        ], ignore_index=True)
+    df_result = pd.DataFrame(json_blueprint["question3"]) if 'question3' in json_blueprint else None
+    df_raw =  pd.DataFrame(json_blueprint["raw_data_report"]) if "raw_data_report" in json_blueprint else None
+    return df_info,df_result,df_raw
+
+def results_table(df_result,result_name='result_name',results_conditions='results_conditions'):
+    unique_result_names = df_result[result_name].unique()
+    unique_conditions = set(condition for conditions in df_result[results_conditions] for condition in conditions)
+    new_header = list(["Material"]) + list(unique_conditions) + list(unique_result_names)
+    return pd.DataFrame(columns=new_header)
+
+def iom_format_2excel(file_path, df_info,df_result,df_raw=None):
     with pd.ExcelWriter(file_path, engine='xlsxwriter') as writer:
-        df.to_excel(writer, sheet_name='Test_conditions', index=False)
+        df_info.to_excel(writer, sheet_name='Test_conditions', index=False)
         workbook = writer.book
         worksheet = writer.sheets['Test_conditions']
-        cell_format = workbook.add_format({'bold': True, 'font_color': 'red'})
-                    # Apply the formatting to the header row
-        for col_num, value in enumerate(df.columns.values):
-            worksheet.write(0, col_num, value, cell_format)
 
         cell_format_header = workbook.add_format({'bold': True, 'font_color': 'black'})
         cell_format_right_align = workbook.add_format({'align': 'right'})
 
         # Apply the formatting to the header row
-        for col_num, value in enumerate(df.columns.values):
+        for col_num, value in enumerate(df_info.columns.values):
             worksheet.write(0, col_num, value, cell_format_header)
-        for row_num in range(1, len(df) + 1):
-            worksheet.write(row_num, 0, df.iloc[row_num - 1, 0], cell_format_right_align)
+        for row_num in range(1, len(df_info) + 1):
+            worksheet.write(row_num, 0, df_info.iloc[row_num - 1, 0], cell_format_right_align)
+
+        #worksheet = writer.sheets['Raw_data']
+
+        #worksheet = writer.sheets['Results']
+        if df_raw is None:
+            pass
+        else:
+            new_df = results_table(df_raw,result_name='raw_endpoint',results_conditions='raw_conditions')
+            new_df.to_excel(writer, sheet_name='Raw_data', index=False)
+        if df_result is None:
+            pass
+        else:
+            new_df = results_table(df_result,result_name='result_name',results_conditions='results_conditions')
+            new_df.to_excel(writer, sheet_name='Results', index=False)
+
+        #worksheet = writer.sheets['Materials']
