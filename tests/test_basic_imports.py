c2900b0650c682f07feaeea28bfdaa723fad605c
vedina <jeliazkova.nina@gmail.com>
2024-08-20 18:27:16 +0300

    use pyambit dependency

--- tests/test_basic_imports.py
+++ tests/test_basic_imports.py
@@ -5,4 +5,4 @@ def test_import():
     from pynanomapper import client_ambit
     from pynanomapper import client_solr
     from pynanomapper import units
-    from pynanomapper.datamodel import ambit
+    from pyambit.datamodel import ambit
